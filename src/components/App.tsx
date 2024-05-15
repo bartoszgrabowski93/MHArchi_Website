7af175af02e05479f4357742b3ce92064d53688e
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-05-15 16:26:08 +0200

    Changes in architecture

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
+import img1 from "../images/pexels-dom-j-7304-310479.jpg";
+import img2 from "../images/pexels-lina-1841147.jpg";
+import img3 from "../images/pexels-pixabay-273209.jpg";
 import "../Styles/Style.css";
 
-function MainPage() {
-  return (
-    <>
-      <div className="container">
-        <div className="containerEmpty"></div>
-        <div className="containerMainPage">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div className="containerEmpty"></div>
-        <div></div>
-      </div>
-    </>
-  );
-}
+const IMAGES = [img1, img2, img3];
 
-export default MainPage;
+export default function MainPage() {
+  return <ImageSlider imageUrls={IMAGES} />;
+}
